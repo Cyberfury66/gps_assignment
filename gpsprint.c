e856d33980d7d330a74bee03a6fb43606c39cfad
mikegoll <mike_g97@hotmail.com>
2016-10-19 16:05:03 +0000

    out of time

--- gpsprint.c
+++ gpsprint.c
@@ -36,7 +36,6 @@
 bool usedflags[MAXCHANNELS];
 struct gps_data_t *gpsdata;
 char *deg_to_str(enum deg_str_type type, double f); 
-void utcTime(struct gps_data_t *gpsdata);
 
 /*------------------------------------------------------------------------------------------------------------------
 -- FUNCTION: printSat
@@ -66,7 +65,7 @@ void printSat(struct gps_data_t *gpsdata) {
 	static enum deg_str_type deg_type = deg_dd;
 	int i, j;
 	
-	for (i = 0; i < satellites_visible; i++) {
+	for (i = 0; i < gpsdata->satellites_visible; i++) {
 		usedflags[i] = false;
 		for (j = 0; j < gpsdata->satellites_used; j++)
 			if (gpsdata->used[j] == gpsdata->PRN[i])
@@ -85,7 +84,6 @@ void printSat(struct gps_data_t *gpsdata) {
 		}
 	}
 
-	void utcTime(gpsdata);
 	
 	if (gpsdata->fix.mode >= MODE_2D && isnan(gpsdata->fix.latitude)==0) {
 		fprintf(stdout, "\nLatitude: %s %c;", deg_to_str(deg_type, fabs(gpsdata->fix.latitude)),
@@ -102,38 +100,4 @@ void printSat(struct gps_data_t *gpsdata) {
 	}
 	else 
 		printf("N/A (Longitude not found)\n");
-}
-
-/*------------------------------------------------------------------------------------------------------------------
--- FUNCTION: utcTime
---
--- DATE: October 19, 2016
---
--- DESIGNER: Yi Hsiang (Mark) Chen
---
--- PROGRAMMER: Yi Hsiang (Mark) Chen
---
--- INTERFACE: utcTime(struct gps_data_t *gpsdata)
---
--- RETURNS: void.
---
--- NOTES:
--- This function converts the timestamp from the GPS to UTC time.
-----------------------------------------------------------------------------------------------------------------------*/
-
-void utcTime(struct gps_data_t *gpsdata){
-	
-	time_t gpsTime;
-	struct tm *utcTime;
-	
-	if (gpsdata->fix.mode >= MODE_2D && isnan(gpsdata->fix.time)==0) {
-		gpsTime = (time_t)gpsTime->fix.time;
-		
-		utcTime = gmtime(&gpsTime);
-		
-		fprintf(stdout, "\nTimestamp : %4d-%02d-%2d %2d:%2d:%02d"", (utcTime->tm_year) + 1900, 
-		(utcTime->tm_mon), (utcTime->tm_mday),(utcTime->tm_hour)%24, (utcTime->tm_min), (utcTime->tm_sec))
-		}
-	else 
-		printf("N/A (Timestamp not available)\n");
 }
